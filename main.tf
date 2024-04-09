c1a8031f667c6eb2983e9f155043cb324c93ce36
s poorna chandra <poornanaidu1@gmail.com>
2024-04-09 12:42:09 +0530

    changes

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "tf-state-poorna"
+    key    = "ami/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
 data "aws_ami" "ami" {
   most_recent = true
   name_regex  = "Centos-8-DevOps-Practice"
@@ -29,4 +37,10 @@ resource "null_resource" "commands" {
     "labauto ansible"
     ]
   }
+}
+
+resource "aws_ami_from_instance" "ami" {
+  depends_on = [null_resource.commands]
+  name               = "roboshop-ami-v1"
+  source_instance_id = aws_instance.ami.id
 }
